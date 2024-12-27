ff18234e6b6a11ad5026a716b0ae49e590d64e5e
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2024-12-27 18:32:59 +0530

    update

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
-const Section1 = () => {
+const Section1 = ({home}) => {
   return (
     <div className='w-full h-screen relative'>
-        <div className='overlay w-full h-full sm:pt-[30vw]  sm:pb-[10vh] pt-[5.5vw] pb-[2vw] absolute top-0 left-0 flex flex-col items-center mix-blend-difference text-white justify-between text-center'>
-           <div>
-           <h2 className='sm:text-[11vw] whitespace-nowrap text-[6vw] leading-[.9] font-["font2"] font-medium tracking-tighter'>365 days of <br /> pushing boundaries <br /> together</h2>
-           <h4 className='sm:text-[4.5vw] text-[1.4vw] sm:mt-[6vw] mt-[2.5vw] font-[font3] font-regular'>Our Partnership Year in Review</h4>
-           </div>
-            <h1 className='sm:text-[23vw] text-[15.5vw] sm:whitespace-normal whitespace-nowrap font-["font2"] font-medium sm:tracking-[-6px] tracking-[-15px] leading-none'>2024 Wrapped</h1>
+      <div ref={home} className='overlay w-full h-full sm:pt-[30vw]  sm:pb-[10vh] pt-[5.5vw] pb-[2vw] absolute top-0 left-0 flex flex-col items-center mix-blend-difference text-white justify-between text-center'>
+        <div>
+          <div className='h-[5.6vw] overflow-hidden'>
+            <h2 style={{transform:"translateY(120%)"}} className='sm:text-[11vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>365 days of</h2>
+          </div>
+          <div className='h-[5.6vw] overflow-hidden'>
+            <h2  style={{transform:"translateY(120%)"}} className='sm:text-[11vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>pushing boundaries</h2>
+          </div>
+          <div className='h-[5.6vw] overflow-hidden'>
+            <h2  style={{transform:"translateY(120%)"}} className='sm:text-[11vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>together</h2>
+          </div>
+          <h4 className='sm:text-[4.5vw] text-[1.4vw] sm:mt-[6vw] mt-[2.5vw] font-[font3] font-regular opacity-0'>Our Partnership Year in Review</h4>
         </div>
+        <div className='h-[16.5vw] w-full overflow-hidden'>
+        <h1  style={{transform:"translateY(120%)"}} className='sm:text-[23vw] text-[16vw] sm:whitespace-normal whitespace-nowrap font-["font2"] font-medium sm:tracking-[-6px]  leading-[.9]'>2024 Wrapped</h1>
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,25 +9,71 @@ gsap.registerPlugin(ScrollToPlugin)
 const index = () => {
   const videoCover = useRef()
   const footer = useRef()
+  const home = useRef()
 
-  useEffect(()=>{
+  useEffect(() => {
+
+
     document.title = "The Hollywood Reporter | Year in Review"
-    gsap.to(window,{
-      scrollTo:0,
+    gsap.to(window, {
+      scrollTo: 0,
     })
-  },[])
+    gsap.set(".main", { height: "100vh" })
+    var tl = gsap.timeline()
+    tl
+      .to(".loader-logo", {
+        opacity: 0,
+        delay: 1,
+        duration: .5
+      })
+      .to(".loader-text", {
+        opacity: 1,
+        duration: .5,
+      })
+      .to(".loader", {
+        top: "-100%",
+        delay: .8,
+        duration: 1
+      })
+      .to(home.current.querySelectorAll("h2"), {
+        transform: "translateY(0%)",
+        stagger: 0.2,
+        duration: .5,
+        delay: -.7,
+
+      }, "a")
+      .to(home.current.querySelector("h4"), {
+        opacity: 1,
+        duration: .5,
+        delay: .1,
+      }, "a")
+      .to(home.current.querySelector("h1"), {
+        transform: "translateY(0%)",
+        delay: -.7,
+        duration: .5,
+        onComplete: function () {
+          gsap.set(".main", { height: "auto" })
+
+        }
+      }, "a")
+
+  }, [])
   return (
     <div className='w-full relativ'>
-      <div className='bg w-full h-screen fixed top-0 left-0'>
+      <div className='bg w-full bg-black h-screen fixed top-0 left-0'>
         <div ref={videoCover} className='h-screen w-full bg-[#000000c1] opacity-0 absolute top-0 left-0'></div>
-      <video autoPlay playsInline muted loop className='w-full h-full object-cover' src="https://video.wixstatic.com/video/84770f_5d5600c9d7d545b5aac32125ff6c68a0/1080p/mp4/file.mp4"></video>
+        <video autoPlay playsInline muted loop className='w-full h-full object-cover' src="https://video.wixstatic.com/video/84770f_5d5600c9d7d545b5aac32125ff6c68a0/1080p/mp4/file.mp4"></video>
       </div>
-   <div className='w-full relative overflow-hidden'>
-    <Section1/>
-    <Section2 videoCover={videoCover} footer={footer}/>
-    <Footer footer={footer}/>
-   </div>
-   </div>
+      <div className='loader w-full h-screen fixed top-0 left-0 bg-white z-[999] flex items-center justify-center flex-col'>
+        <img src="/thr-logo.png" className='loader-logo w-[15%] absolute top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2' alt="" />
+        <h4 className='loader-text font-[font3] uppercase text-[#DE1F26] text-[1.5vw] font-semibold opacity-0'>The Wrapped 2024</h4>
+      </div>
+      <div className='main  relative overflow-hidden'>
+        <Section1 home={home} />
+        <Section2 videoCover={videoCover} footer={footer} />
+        <Footer footer={footer} />
+      </div>
+    </div>
   )
 }
 
