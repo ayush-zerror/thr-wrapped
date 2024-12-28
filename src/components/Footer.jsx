fae05c32042302872ff72a8b5e3b5a8db894d86c
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2024-12-28 12:03:03 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = ({footer}) => {
                 <h1 className='sm:text-[25vw] text-[18.5vw] sm:whitespace-normal whitespace-nowrap font-["font2"] uppercase leading-[.7] w-fit font-medium'>Onward 2025</h1>
                 <div className='text-center'>
                     <h5 className='text-[.98vw] sm:hidden uppercase font-[font3] font-medium'>RPSG Lifestyle Media Thapar House, 3rd floor,<br /> Janpath Lane, New Delhi - 110001.</h5>
-                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>500 Terry Francine Street,</h5>
-                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>San Francisco, CA 94158</h5>
+                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>RPSG Lifestyle Media Thapar House, 3rd floor,</h5>
+                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Janpath Lane, New Delhi - 110001.</h5>
                     <h5 className='sm:text-[4vw] sm:font-normal  text-[.98vw] uppercase font-[font3] font-medium flex gap-[2vw] justify-center'>
                         <a href="https://www.facebook.com/hollywoodreporterindia" target='_block'>[FACEBOOK]</a>
 
@@ -15,9 +15,9 @@ const Footer = ({footer}) => {
 
                         <a href="https://www.youtube.com/@HollywoodReporterIndia" target='_block'>[YOUTUBE]</a>
                     </h5>
-                    <h5 className='sm:text-[4vw] sm:font-normal sm:mt-[10vw] sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Privacy Policy</h5>
+                    <a href='https://www.hollywoodreporterindia.com/privacy-policy'  target='_block' className='sm:text-[4vw] sm:font-normal sm:mt-[10vw] sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Privacy Policy</a>
                     <a href='https://www.hollywoodreporterindia.com/disclaimer' target='_block' className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Disclaimer</a>
-                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>© 2035 by so.far. Built on Wix Studio</h5>
+                    <h5 className='sm:text-[4vw] sm:font-normal sm:block hidden text-[.98vw] uppercase font-[font3] font-medium'>© 2025 The Hollywood Reporter, LLC.</h5>
 
                     <h5 className='sm:hidden  text-[.98vw] mt-[2.5vw] uppercase font-[font3] font-medium flex gap-[2vw] justify-center'><a href='https://www.hollywoodreporterindia.com/privacy-policy' target='_block'>Privacy Policy</a>
 
