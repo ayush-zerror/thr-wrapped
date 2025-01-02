9ee81926045e0b7419b27f2c5399878032d4d1cf
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-02 18:29:57 +0530

    update

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -406,7 +406,7 @@ const Section2 = ({ videoCover, footer }) => {
 
   return (
     <div ref={container} className='w-full min-h-screen py-[5vw]'>
-      {data.map((d, i) => (<div ref={(el) => wrappers.current[i] = el} key={i} className='wrap w-full sm:h-[240px] md:h-[80%] lg:h-[42vh] overflow-hidden  xl:h-[100%]  h-[80%] flex items-start justify-between relative'>
+      {data.map((d, i) => (<div ref={(el) => wrappers.current[i] = el} key={i} className='wrap w-full sm:h-[275px] md:h-[80%] lg:h-[42vh] overflow-hidden  xl:h-[100%]  h-[80%] flex items-start justify-between relative'>
         <div className="line1t bg-white w-full h-[1px] absolute top-0 left-1/2 -translate-x-1/2"></div>
         {data.length - 1 === i && <div className="line1b bg-white/80 w-full h-[1px] z-10 absolute bottom-0 left-0"></div>}
         <div className="card sm:hidden md:hidden p-[3vw] py-[3vw] w-[calc(100%/3)] h-full relative text-white">
@@ -417,9 +417,9 @@ const Section2 = ({ videoCover, footer }) => {
               <h3 className='text-[.9vw] z-20 text-white relative'>Read full article</h3>
             </a>
           </div>
-          <div className="line1l sm:h-[240px] md:h-[80%] lg:h-[42vh] xl:h-[200%] h-[180%] w-[1px] bg-white/80 absolute right-0 top-0"></div>
+          <div className="line1l sm:h-[275px] md:h-[80%] lg:h-[42vh] xl:h-[200%] h-[180%] w-[1px] bg-white/80 absolute right-0 top-0"></div>
         </div>
-        <div style={{ transformOrigin: "top center" }} className="card  card-center relative sm:w-[50%] md:w-[50%] w-[calc(100%/3)] sm:h-[240px] md:h-[80%] lg:h-[80%]  h-full">
+        <div style={{ transformOrigin: "top center" }} className="card  card-center relative sm:w-[50%] md:w-[50%] w-[calc(100%/3)] sm:h-[275px] md:h-[80%] lg:h-[80%]  h-full">
          <div className='relative overflow-hidden'>
          <Image  width={1000} height={1000} className='object-cover' src={d.poster} alt=""/>
          </div>
@@ -432,8 +432,8 @@ const Section2 = ({ videoCover, footer }) => {
           </div>
         </div>
         <div className="card  sm:w-[50%]  md:w-[50%] w-[calc(100%/3)]  relative text-white">
-          <div className="line1r sm:h-[240px] md:h-[120%] lg:h-[42vh] xl:h-[200%] h-[710px] w-[1px] bg-white/80 absolute left-0 top-0"></div>
-          <div className='card-content scrollstyle  p-[3vw] py-[3vw] sm:h-[240px] md:h-[80%] lg:h-[80%] xl:h-[100%]  overflow-y-scroll overflow-hidden pointer-events-none'>
+          <div className="line1r sm:h-[275px] md:h-[120%] lg:h-[42vh] xl:h-[200%] h-[710px] w-[1px] bg-white/80 absolute left-0 top-0"></div>
+          <div className='card-content scrollstyle  p-[3vw] py-[3vw] sm:h-[275px] md:h-[80%] lg:h-[80%] xl:h-[100%]  overflow-y-scroll overflow-hidden pointer-events-none'>
             {d.details.map((dets, i) => ( <h4 key={i} className='sm:text-[3.3vw] md:text-[2.5vw] lg:text-[1.8vw] text-[1.3vw] mb-[1.2vw] font-[font3] font-regular flex items-center sm:gap-[1vw] gap-[.5vw]'><span className='sm:w-[1vw] md:w-[.7vw] w-[.3vw] flex-shrink-0 sm:h-[1vw] md:h-[.7vw] h-[.3vw] block bg-white rounded-full'></span> {dets}</h4>))}
           </div>
         </div>
