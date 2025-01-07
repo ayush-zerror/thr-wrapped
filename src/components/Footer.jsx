81333a4112993a09b39219b03e5fd1f66ce5ba5f
ayush.zerror@gmail.com <ayush.zerror@gmail.com>
2025-01-07 12:35:34 +0530

    text content update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 const Footer = ({ footer }) => {
     return (
         <div ref={footer} className='footer w-full h-screen relative'>
             <div className='overlay w-full h-full  absolute top-0 left-0 flex flex-col items-center text-white justify-between text-center p-[1.5vw] sm:pt-[15vh] sm:py-[8vw]'>
-                <h1 className='sm:text-[25vw] md:text-[22vw] text-[18.5vw] sm:whitespace-normal md:whitespace-normal  whitespace-nowrap font-["font2"] uppercase leading-[.7] w-fit font-medium'>Onward 2025</h1>
-                <h5 className='lg:text-[1.5vw] text-[.98vw] sm:hidden md:hidden uppercase  font-[font3] font-medium w-[55%]  mb-[15vw]'>2025 promises to be another pivotal year at the movies, with several big-budget releases, intriguing collaborations, as well as burgeoning indie breakouts and dark horses in the mix. Stay tuned to our website for the best of in-depth reportage, interviews, reviews, box-office analysis, and curated opinions on all we imagine as cinema in India.</h5>
+                <h1 className='sm:text-[25vw] md:text-[22vw] text-[18.5vw] sm:whitespace-normal md:whitespace-normal  whitespace-nowrap font-["font2"]  leading-[.7] w-fit font-medium'>Onward 2025</h1>
+                <h5 className='sm:text-[3.4vw] lg:text-[2vw] xl:text-[1.6vw] md:text-[2.5vw] text-[1vw] font-[font3] font-normal sm:w-[85%] md:w-[75%] lg:w-[70xl xl:w-[70%] w-[48%] md:mb-[35vw] lg:mb-[36vw] xl:mb-[30vw]  mb-[15vw]'>2025 promises to be another pivotal year at the movies, with several big-budget releases, intriguing collaborations, as well as burgeoning indie breakouts and dark horses in the mix. Stay tuned to our website for the best of in-depth reportage, interviews, reviews, box-office analysis, and curated opinions on all we imagine as cinema in India.</h5>
                 <div className='text-center'>
-                    <h5 className='lg:text-[1.5vw] text-[.98vw] sm:hidden md:hidden uppercase font-[font3] font-medium'>RPSG Lifestyle Media Thapar House, 3rd floor,<br /> Janpath Lane, New Delhi - 110001.</h5>
-                    <h5 className='sm:text-[4vw] md:text-[2vw] sm:font-normal sm:block md:block hidden text-[.98vw] uppercase font-[font3] font-medium'>RPSG Lifestyle Media Thapar House, 3rd floor,</h5>
-                    <h5 className='sm:text-[4vw] md:text-[2vw] sm:font-normal sm:block md:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Janpath Lane, New Delhi - 110001.</h5>
-                    <h5 className='sm:text-[4vw] md:text-[2vw] lg:text-[1.5vw] sm:font-normal  text-[.98vw] uppercase font-[font3] font-medium flex gap-[2vw] justify-center'>
-                        <a href="https://www.facebook.com/hollywoodreporterindia" target='_block'>[FACEBOOK]</a>
+                    <h5 className='lg:text-[1.5vw] xl:text-[1.5vw] text-[.98vw] sm:hidden md:hidden  font-[font3] font-normal'>RPSG Lifestyle Media Thapar House, 3rd floor,<br /> Janpath Lane, New Delhi - 110001.</h5>
+                    <h5 className='sm:text-[3.2vw] md:text-[2vw]  sm:font-normal sm:block md:block hidden text-[.98vw]  font-[font3] font-normal'>RPSG Lifestyle Media Thapar House, 3rd floor,</h5>
+                    <h5 className='sm:text-[3.2vw] md:text-[2vw]  sm:mb-[2.5vw] sm:font-normal sm:block md:block hidden text-[.98vw]  font-[font3] font-normal'>Janpath Lane, New Delhi - 110001.</h5>
+                  
+                    <h5 className=' sm:text-[3.2vw] md:text-[2vw] sm:block md:block hidden text-[.98vw] lg:text-[1.5vw] sm:mb-[1vw] font-[font3] font-normal'><Link href="https://www.hollywoodreporterindia.com/">Visit The Hollywood Reporter India to explore.</Link></h5>
+                    <h5 className=' sm:text-[3.2vw] md:text-[2vw] sm:block md:block hidden text-[.98vw] lg:text-[1.5vw]  font-[font3] font-normal'>© 2025 The Hollywood Reporter, LLC.</h5>
 
-                        <a href="https://www.instagram.com/hollywoodreporterindia" target='_block'>[INSTAGRAM]</a>
-
-                        <a href="https://www.youtube.com/@HollywoodReporterIndia" target='_block'>[YOUTUBE]</a>
-                    </h5>
-                    <a href='https://www.hollywoodreporterindia.com/privacy-policy' target='_block' className='sm:text-[4vw] md:text-[2vw] sm:font-normal sm:mt-[10vw] sm:block md:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Privacy Policy</a>
-                    <a href='https://www.hollywoodreporterindia.com/disclaimer' target='_block' className='sm:text-[4vw] md:text-[2vw] sm:font-normal sm:block md:block hidden text-[.98vw] uppercase font-[font3] font-medium'>Disclaimer</a>
-                    <h5 className='sm:text-[4vw] md:text-[2vw] sm:font-normal sm:block md:block hidden text-[.98vw] lg:text-[1.5vw] uppercase font-[font3] font-medium'>© 2025 The Hollywood Reporter, LLC.</h5>
-
-                    <h5 className='sm:hidden md:hidden  text-[.98vw] lg:text-[1.5vw] mt-[2.5vw] uppercase font-[font3] font-medium flex gap-[2vw] justify-center'><a href='https://www.hollywoodreporterindia.com/privacy-policy' target='_block'>Privacy Policy</a>
-
-                        <a href='https://www.hollywoodreporterindia.com/disclaimer' target='_block'>Disclaimer</a>
-
-                        <span>© 2025 The Hollywood Reporter, LLC.</span></h5>
+                    <h5 className='sm:hidden md:hidden  text-[.98vw] lg:text-[1.5vw] xl:text-[1.5vw] mt-[2.2vw]  font-[font3] font-normal flex gap-[2.8vw] justify-between w-full'><span>© 2025 The Hollywood Reporter, LLC.</span> <Link href="https://www.hollywoodreporterindia.com/">Visit The Hollywood Reporter India to explore.</Link></h5>
                 </div>
             </div>
         </div>
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 const Section1 = ({home}) => {
   return (
     <div className='w-full h-screen relative'>
-      <div ref={home} className='overlay w-full h-full sm:pt-[30vw] md:pt-[10vw]  sm:pb-[10vh] pt-[2.5vw] pb-[2vw] absolute top-0 left-0 flex flex-col items-center  text-white justify-between text-center'>
+      <div ref={home} className='overlay w-full h-full sm:pt-[15vw] md:pt-[10vw]  sm:pb-[3vh] pt-[1.8vw] pb-[1.5vw] absolute top-0 left-0 flex flex-col items-center  text-white justify-between text-center'>
         <div>
-          <div className='sm:h-[15vw] md:h-[9vw] lg:h-[8.5vw] h-[6.5vw] overflow-hidden'>
-            <h2 style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[8vw] lg:text-[8vw] whitespace-nowrap text-[6.5vw] leading-[.9] font-["font2"] font-medium '>365 days of</h2>
+          <div className='sm:h-[15vw] md:h-[10vw] lg:h-[8.5vw] h-[5.5vw] overflow-hidden'>
+            <h2 style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[10vw] lg:text-[8vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>365 days of</h2>
           </div>
-          <div className='sm:h-[15vw] md:h-[9vw] lg:h-[8.5vw] h-[6.5vw] overflow-hidden'>
-            <h2  style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[8vw] lg:text-[8vw] whitespace-nowrap text-[6.5vw] leading-[.9] font-["font2"] font-medium '>pushing boundaries</h2>
+          <div className='sm:h-[15vw] md:h-[10vw] lg:h-[8.5vw] h-[5.5vw] overflow-hidden'>
+            <h2  style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[10vw] lg:text-[8vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>pushing boundaries</h2>
           </div>
-          <div className='sm:h-[15vw] md:h-[9vw] lg:h-[8.5vw] h-[6.5vw] overflow-hidden'>
-            <h2  style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[8vw] lg:text-[8vw] whitespace-nowrap text-[6.5vw] leading-[.9] font-["font2"] font-medium '>together</h2>
+          <div className='sm:h-[15vw] md:h-[10vw] lg:h-[8.5vw] h-[5.5vw] overflow-hidden'>
+            <h2  style={{transform:"translateY(140%)"}} className='sm:text-[14vw] md:text-[10vw] lg:text-[8vw] whitespace-nowrap text-[5.5vw] leading-[.9] font-["font2"] font-medium '>together</h2>
           </div>
-          <h4 className='sm:text-[6vw] lg:text-[2.2vw] md:text-[3vw] text-[1.4vw] sm:mt-[6vw] md:mt-[3vw] mt-[1.5vw] font-[font3] font-regular opacity-0'>Our Partnership Year in Review</h4>
+          <h4 className='sm:text-[3.2vw] lg:text-[2vw] xl:text-[1.6vw] md:text-[2.5vw] text-[1vw] sm:w-[85%] md:w-[80%] lg:w-[80%] xl:w-[75%] w-[48%] mx-auto sm:mt-[6vw] md:mt-[3vw] mt-[1.5vw] font-[font3] font-normal opacity-0'>Curated by the editorial team of The Hollywood Reporter India, here is a comprehensive deep-dive into the best of 2024 across Indian film industries, with our range of year-enders that encompass everything from best films and performances to star fees and box-office figures. We hope you enjoy looking back at the year that was in Indian cinema.
+          </h4>
         </div>
-        <div className='sm:h-[49vw] md:h-[35vw] h-[14vw] w-full overflow-hidden'>
+        <div className='sm:h-[52vw] md:h-[35vw] h-[14vw] w-full overflow-hidden'>
         <h1  style={{transform:"translateY(140%)"}} className='sm:text-[25vw] md:text-[20vw] lg:text-[18vw] text-[18vw] uppercase  sm:whitespace-normal  md:whitespace-normal whitespace-nowrap font-["font2"] font-medium  leading-[.9]'>2024 Wrapped</h1>
         </div>
       </div>
